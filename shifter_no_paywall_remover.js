aab09054cc5a4fe23e9aaf6043d74c397aa216cc
Morten Olsen Lysgaard <morten@lysgaard.no>
2022-04-27 09:06:41 +0200

    update shifter script to handle more of the fluff

--- shifter_no_paywall_remover.js
+++ shifter_no_paywall_remover.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         shifter_no_paywall_remover.js
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  remove soft paywall on shifter.no
 // @author       Morten Lysgaard
 // @match        *://shifter.no/*
@@ -12,12 +12,30 @@
 (function() {
     'use strict';
 
-    (function init(){
+    // Removes offer to subscribe blocking reading of articles
+    (function subscribe_block(){
         var el = document.querySelector('.boksen--active');
         if (el) {
             el.remove();
         } else {
-            setTimeout(init, 100);
+            setTimeout(subscribe_block, 100);
+        }
+    })();
+
+    // Removes modal that asks for newsletter subscription and blocks whole screen
+    (function newsletter_block(){
+        var el = document.querySelector('.tp-close .tp-active');
+          if (el) {
+            var evt = new MouseEvent("click", {
+                view: window,
+                bubbles: true,
+                cancelable: true,
+                clientX: 20,
+                /* whatever properties you want to give it */
+            });
+            el.dispatchEvent(evt);
+        } else {
+            setTimeout(newsletter_block, 100);
         }
     })();
 })();
